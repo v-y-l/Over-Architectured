72f7504457e8efc8bc0e2285587fea3f214e615c
Victor Lin <vlinvlin6@gmail.com>
2024-01-18 20:37:31 -0500

    Add postgresql stub

---
+++ postgres/schema/movie.sql
@@ -0,0 +1,12 @@
+CREATE TABLE movie (
+ movie_id SERIAL PRIMARY KEY,
+ movie_name VARCHAR(50) NOT NULL,
+);
+
+-- Affects inserts, updated, deletes, but improves look-up
+-- e.g. WHERE, JOIN, ORDER BY which used to require sequential
+-- scan.
+--
+-- We're essentially using a b-tree here using varchar order,
+-- whose value points at the data page with the row.
+CREATE INDEX movie_name_index ON movie(move_name);
